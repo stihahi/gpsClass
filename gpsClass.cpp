77b857fce86369fc1bf6dd2b978b4672b4f9361e
Satoshi Iwai <stihahi@gmail.com>
2014-07-30 23:06:59 +0900

    Merge branch 'RMCParser'
    merged RMCParser

--- gpsClass.cpp
+++ gpsClass.cpp
@@ -9,6 +9,7 @@
 #include "gpsClass.h"
 #include "Arduino.h"
 
+
 void gpsClass::auto_detect_baud_rate(void){
     const unsigned int bauds[] = {
         57600, 38400, 28800, 14400, 9600, 4800 };
@@ -46,7 +47,7 @@ void gpsClass::serialSetup(void){
     char sendPack[256];
     sprintf(sendPack,"PMTK220,%d",reloadSec * 1000);//秒数
     send_pmtk_packet(sendPack);
-    send_pmtk_packet("PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0");
+    send_pmtk_packet("PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0");
     send_pmtk_packet("PMTK251,9600");
     auto_detect_baud_rate();
 }
@@ -65,7 +66,7 @@ char* gpsClass::gpsFetch(void){
         unsigned long last_time = millis();
         do{
             char c=read();
-            if (c == '$') {
+            if (c == '$') {//'$' is cut at the first loop.
                 preamble ++;
                 index = 0;
                 while (c != '\n') {
@@ -86,10 +87,101 @@ char* gpsClass::gpsFetch(void){
     return "";
 }
 
-void gpsClass::parser(float &latitude,float &longitude,char *readData){
+bool gpsClass::parser(char *readData){
     int length = strlen(readData);
     for (int k=0; k<length; k++) {
         Serial.print(readData[k]);
+    }
+    if (length != 0) {
+        int datType = headerParser(readData);//ヘッダで、タイプわけする。
+        Serial.print("dat type:");
+        Serial.println(datType);
+        RMCParser(readData,rmcD);
+//        Serial.println(rmcD.serialShow());
+        return true;
+    }
+    return false;
+}
+
+int gpsClass::headerParser(char *readData){
+    char *c = readData;
+    char headPhrase[10];
+    int index = 0;
+    while (*c != ',') {
+        headPhrase[index++] = *c;
+        c++;
+    }
+    headPhrase[index] = 0;
+    if (strcmp(headPhrase,"GPRMC") == 0) {
+        return NMEA_RMC;
+    }else if(strcmp(headPhrase,"GPGGA") == 0){
+        return NMEA_GGA;
+    }else if(strcmp(headPhrase,"GPZDA") == 0){
+        return NMEA_ZDA;
+    }
+    return NMEA_OTHER;
+}
+
+
+void gpsClass::RMCParser(char *readData,RMCData &rmc){
+    char *c = readData;
+    char nowPhrase[50];
+    int index = 0;
+    int dataNum = 0;
+    while (*c != 0) {
+        if (*c != ',') {
+            nowPhrase[index++] = *c;
+        }else{
+            nowPhrase[index++] = 0;
+/*            Serial.print("dataNum:");Serial.print(dataNum);
+            Serial.print("[");Serial.print(nowPhrase);Serial.println("]");*/
+            switch (dataNum) {
+                case 0:
+                    if (strcmp(nowPhrase,"GPRMC") != 0) {
+                        return;//データの種類が違う~!
+                    }
+                    break;
+                case 1://225446.00	＝　測位時刻（UTC）　22:54:46.00
+                    rmc.hour = CTOI(nowPhrase[0])*10 + CTOI(nowPhrase[1]);
+                    rmc.min = CTOI(nowPhrase[2])*10 + CTOI(nowPhrase[3]);
+                    rmc.sec = CTOI(nowPhrase[4])*10 + CTOI(nowPhrase[5]);
+                    break;
+                case 2://A	＝　ステータス；A = 有効，V = 無効
+                    if (nowPhrase[0] == 'A') {
+                        rmc.status = true;
+                    }else{
+                        rmc.status = false;
+                    }
+                    break;
+                case 3://4916.452653,N	＝　緯度　49度16.452653分（北緯）
+                    rmc.latitude = atof(nowPhrase);
+                    break;
+                case 4:
+                    if (nowPhrase[0] == 'S') {
+                        rmc.latitude = -rmc.latitude;
+                    }
+                    break;
+                case 5://12311.123747,W	＝　経度　123度11.123747分（西経）
+                    rmc.longitude = atof(nowPhrase);
+                    break;
+                case 6:
+                    if (nowPhrase[0] == 'W') {
+                        rmc.longitude = -rmc.longitude;
+                    }
+                    break;
+                case 7://000.5	＝　対地速度（ノット）　0.5ノット
+                    rmc.knot = atof(nowPhrase);
+                    break;
+                case 8://054.7	＝　進行方向（度，真北）　54.7度
+                    rmc.heading = atoi(nowPhrase);
+                    break;
+                default:
+                    break;
+            }
+            dataNum ++;
+            index = 0;
+        }
+        c++;
     }
 }
 
@@ -111,3 +203,44 @@ void gpsClass::send_pmtk_packet(char *p)
     print('*');
     println(checksum,HEX);
 }
+
+char* gpsClass::getLCD(int mode,int line){
+    switch (mode) {
+        case NMEA_RMC:
+            return rmcD.lcdShow(line);
+            break;
+            
+        default:
+            return "";
+            break;
+    }
+}
+
+char* RMCData::serialShow(void){
+    char ret[256];
+    char NS;
+    char WE;
+    char temp1[12],temp2[12],temp3[12];
+    if (latitude >0) {NS = 'N';}else{NS='S';}
+    if (longitude > 0){WE = 'E';}else{WE='W';}
+    sprintf(ret,"%d:%d:%d status:%d %c%s %c%s speed:%s heading:%d",hour,min,sec,status,NS,dtostrf(latitude,9,4,temp1),WE,dtostrf(longitude,10,4,temp2),dtostrf(knot,7,4,temp3),heading);
+    return ret;
+}
+
+char* RMCData::lcdShow(int line){
+    if (!status) {
+        return "00000000";
+    }
+    char ret[256];
+    char NSWE;
+    char temp1[12];
+    if (line == 0) {
+        if (latitude >0) {NSWE = 'N';}else{NSWE='S';}
+        sprintf(ret,"%c%s",NSWE,dtostrf(latitude/100,8,6,temp1));
+    }else{
+        if (longitude > 0){NSWE = 'E';}else{NSWE='W';}
+        sprintf(ret,"%c%s",NSWE,dtostrf(longitude/100,8,5,temp1));
+    }
+    return ret;
+}
+
--- gpsClass.h
+++ gpsClass.h
@@ -1,6 +1,6 @@
 //
 //  gpsClass.h
-//  
+//
 //
 //  Created by 岩井聡 on 2014/07/28.
 //
@@ -10,11 +10,35 @@
 #define ____gpsClass__
 
 #include <SoftwareSerial.h>
+#define CTOI(A) (A-'0')
+
+enum NMEA0183{NMEA_OTHER,NMEA_RMC,NMEA_ZDA,NMEA_GGA};
+
+class gpsData{
+public:
+    char* lcdShow(int line);//データを整形して返すメソッド。画面表示用。
+    char* serialShow(void);
+};
+
+struct RMCData : public gpsData{
+    char hour,min,sec;
+    char day,month,year;
+    bool status;
+    float latitude;
+    float longitude;
+    float knot;
+    int heading;
+    char* lcdShow(int line);//データを整形して返すメソッド。画面表示用。
+    char* serialShow(void);//データを整形してシリアルポートに返す。
+    RMCData(){status = false;};
+};
 
 class gpsClass : public SoftwareSerial{
     int reloadSec;
     void auto_detect_baud_rate(void);
     void send_pmtk_packet(char *p);
+    int headerParser(char *readData);//private class that determins the type of data.
+    RMCData rmcD;
 public:
     gpsClass(int a,int b) : SoftwareSerial(a,b){
         reloadSec = 1;//default 5 sec
@@ -22,7 +46,12 @@ public:
     void serialSetup(void);
     void readData(void);
     char* gpsFetch(void);
-    void parser(float &latitude,float &longitude,char *readData);
+    bool parser(char *readData);
+    void RMCParser(char *readData,RMCData &rmc);
+    char* getLCD(int mode,int line);
 };
+
+
+
 
 #endif /* defined(____gpsClass__) */
